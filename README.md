bb49213211a5b538239a184c31e2d06286cb53fb
Jamey McElveen <jamey@mcelveen.us>
2017-04-17 19:16:30 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Particle CLI
+particle command line tool
+
+### Build
+```
+# Install yarn from [https://yarnpkg.com](https://yarnpkg.com)
+$ yarn install
+$ npm build
+```
+
---
+++ bin/part-cli.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const part = require('../lib/part.js');
+
+//const rootPath = path.resolve(process.cwd());
+
+
+part();
+
+
+
+
+/*
+ Usage: custom-help [options]
+
+ Options:
+
+ -h, --help     output usage information
+ -V, --version  output the version number
+ -f, --foo      enable some foo
+ -b, --bar      enable some bar
+ -B, --baz      enable some baz
+ */
